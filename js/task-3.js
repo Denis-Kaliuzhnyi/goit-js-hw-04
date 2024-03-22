3a26e5f1788925c6a6ac888afe21ddf3ec1afff0
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-03-22 15:24:52 +0200

    task-4.3

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,32 @@
-function filterArray(numbers, value) {
-   let newFilterArray = []; 
-   for (let i = 0; i < numbers.length; i++) {
-   if (numbers[i] > value) {
-   newFilterArray.push(numbers[i]);}
-   }
-   return newFilterArray; }
+const profile = {
+username: "Jacob",
+playTime: 300,
+changeUsername(newName) {
+   profile.username = newName;
+},
+updatePlayTime(hours) {
+   profile.playTime += hours;
+},
+getInfo() {
+   return `${profile.username} has ${profile.playTime} active hours!`;
+}}
 
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
+// Об’єкт profile описує профіль користувача на ігровій платформі. У його властивостях зберігається ім’я профілю username та кількість активних годин playTime, проведених у грі.
+
+// Доповни об’єкт profile методами для роботи з його властивостями.
+
+// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
+// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
